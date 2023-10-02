a8fd2eac8a011ba89acc34d65936fcdb317be27a
Nazila Malekzadah <Nazilamalekzadah4@gmail.com>
2023-10-02 21:11:21 +0100

    fast forward merging

---
+++ average.py
@@ -0,0 +1,9 @@
+# Define a list of numbers
+numbers = [5, 10, 15, 20, 25]
+
+# Calculate the average
+if len(numbers) > 0:
+    average = sum(numbers) / len(numbers)
+    print(f"The average of the numbers is: {average}")
+else:
+    print("No numbers to calculate the average.")
