dd4d72004702b8223c8da95b6756071c188e2164
bicoupler <138819756+bicoupler@users.noreply.github.com>
2023-10-04 10:34:46 +0100

    Much Better

--- average.py
+++ average.py
@@ -7,3 +7,15 @@ if len(numbers) > 0:
     print(f"The average of the numbers is: {average}")
 else:
     print("No numbers to calculate the average.")
+
+# FORK START
+large = 0
+small = numbers[0]
+for i in numbers:
+	if(i > large):
+		large = i
+	if(i < small):
+		small = i
+
+print('Largest = ', large)
+print('Smallest = ', small)
